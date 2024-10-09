8cbc6f68553d22fca2e9084ca02fcf53149e65e9
KingVishal703 <vishal21wankhede@gmail.com>
2024-10-09 13:43:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,10 +33,10 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #----------- There will be channel id add in all these ---------
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))  # set shortner log channel
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0')) # The movie you upload in it will be deleted from the bot.
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002207449255'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1002114485322')
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
-request_channel = environ.get('REQUEST_CHANNEL', '0') # If anyone sends a request message to your bot, you will get it in this channel.
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002057941851'))
+request_channel = environ.get('REQUEST_CHANNEL', '-1002057941851') # If anyone sends a request message to your bot, you will get it in this channel.
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002111868028')) # 
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+i6Tw480uKqQ3MDg9') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
