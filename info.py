1b1a3dc4b4766e88b65d116b6c81a675b0407bfb
KingVishal703 <vishal21wankhede@gmail.com>
2024-10-09 11:50:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://external-livvie-moviesking70-ad8c17cf.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
